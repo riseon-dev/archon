d921e369754f59ab4c8372bbace8aa1e57c25911
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-05-05 19:23:31 +0530

    initial spec for create-vault

---
+++ programs/copy-trading/tests/create-vault.spec.ts
@@ -0,0 +1,9 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+import { CopyTrading } from "../target/types/copy_trading";
+
+describe('Create Vault Instruction', () => {
+  it.todo('should create vault');
+  it.todo('should close vault');
+  it.todo('should throw error (of type?) if vault uid already exists');
+})
