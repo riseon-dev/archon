ba66798a27984fbe010864217e7a443d65f48c0a
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-05-10 13:45:47 +0530

    deposit state

--- programs/copy-trading/programs/copy-trading/src/instructions/deposit.rs
+++ programs/copy-trading/programs/copy-trading/src/instructions/deposit.rs
@@ -1,1 +1,36 @@
+pub use crate::constants::*;
+use crate::vault::Vault;
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
+use anchor_spl::token::TokenAccount;
+use anchor_spl::token_2022::Token2022;
+use anchor_spl::token_interface::Mint;
 
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(
+        mut,
+        owner = system_program::ID, // maybe not needed
+    )]
+    pub investor: Signer<'info>,
+
+    #[account(mut)]
+    pub vault: Account<'info, Vault>,
+
+    #[account(
+        mut,
+        seeds = [b"mint", vault.key().as_ref()],
+        bump = vault.mint_bump,
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+    
+    #[account(
+        mut,
+        seeds = [b"vault", vault.key().as_ref()], // probably not correct
+        bump = vault.vault_bump, // probably not correct
+    )]
+    pub mint_to: Account<'info, TokenAccount>,
+
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token2022>,
+}
