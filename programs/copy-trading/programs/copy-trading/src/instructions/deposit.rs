9e9eee5c1d528fb48153cc42e9b4eb62a7178a9b
Abdulla Faraz <abdulla.faraz@gmail.com>
2025-05-13 13:26:33 +0530

    working deposit test

--- programs/copy-trading/programs/copy-trading/src/instructions/deposit.rs
+++ programs/copy-trading/programs/copy-trading/src/instructions/deposit.rs
@@ -4,8 +4,9 @@ use anchor_lang::prelude::*;
 use anchor_lang::solana_program::system_program;
 use anchor_lang::system_program::{transfer, Transfer};
 use anchor_spl::{
-    associated_token::AssociatedToken, token::TokenAccount, token_2022::Token2022,
-    token_interface::Mint,
+    associated_token::AssociatedToken,
+    token_2022::Token2022,
+    token_interface::{Mint, TokenAccount},
 };
 
 // Look at the following repo
@@ -25,23 +26,24 @@ pub struct Deposit<'info> {
         bump = vault.vault_bump
     )]
     pub vault: Account<'info, Vault>,
-    //
-    // #[account(
-    //     mut,
-    //     address = vault.mint,
-    // )]
-    // pub mint: InterfaceAccount<'info, Mint>,
-    //
-    // #[account(
-    //     init_if_needed,
-    //     payer = investor,
-    //     associated_token::mint = mint,
-    //     associated_token::authority = investor,
-    // )]
-    // pub mint_to: Account<'info, TokenAccount>,
-    //
-    // pub associated_token_program: Program<'info, AssociatedToken>,
-    // pub token_program: Program<'info, Token2022>,
+
+    #[account(
+        mut,
+        address = vault.mint,
+    )]
+    pub mint: InterfaceAccount<'info, Mint>,
+
+    #[account(
+        init_if_needed,
+        payer = investor,
+        associated_token::mint = mint,
+        associated_token::authority = investor,
+        associated_token::token_program = token_program,
+    )]
+    pub mint_to: InterfaceAccount<'info, TokenAccount>,
+
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token2022>,
     pub system_program: Program<'info, System>,
     pub rent: Sysvar<'info, Rent>,
 }
@@ -66,37 +68,37 @@ pub fn deposit_sol(ctx: &mut Context<Deposit>, amount: u64) -> Result<()> {
 }
 
 pub fn mint_vault_tokens(ctx: &mut Context<Deposit>, amount: u64) -> Result<()> {
-    // msg!(
-    //     "Minting {} tokens to {}",
-    //     amount,
-    //     ctx.accounts.mint_to.key()
-    // );
-    //
-    // // PDA signer seeds
-    // let operator_key = ctx.accounts.operator.key();
-    // let seeds = &[
-    //     b"vault",
-    //     operator_key.as_ref(),
-    //     &[ctx.accounts.vault.vault_bump],
-    // ];
-    // let signer = &[&seeds[..]];
-    //
-    //
-    // // Mint tokens to the investor
-    // let cpi_accounts = anchor_spl::token_2022::MintTo {
-    //     mint: ctx.accounts.mint.to_account_info(),
-    //     to: ctx.accounts.mint_to.to_account_info(),
-    //     authority: ctx.accounts.vault.to_account_info(),
-    // };
-    // let cpi_ctx = CpiContext::new_with_signer(
-    //     ctx.accounts.token_program.to_account_info(),
-    //     cpi_accounts,
-    //     signer,
-    // );
-    // anchor_spl::token_2022::mint_to(cpi_ctx, amount)?;
-    //
-    // // Update the vault state
-    // ctx.accounts.vault.tokens_issued += amount;
+    msg!(
+        "Minting {} tokens to investor's ATA {}",
+        amount,
+        ctx.accounts.mint_to.key(), // investor's associated token account
+    );
+
+    // PDA signer seeds
+    let operator_key = ctx.accounts.operator.key();
+    let seeds = &[
+        b"vault",
+        operator_key.as_ref(),
+        &[ctx.accounts.vault.vault_bump],
+    ];
+    let signer = &[&seeds[..]];
+
+
+    // Mint tokens to the investor
+    let cpi_accounts = anchor_spl::token_2022::MintTo {
+        mint: ctx.accounts.mint.to_account_info(),
+        to: ctx.accounts.mint_to.to_account_info(),
+        authority: ctx.accounts.vault.to_account_info(),
+    };
+    let cpi_ctx = CpiContext::new_with_signer(
+        ctx.accounts.token_program.to_account_info(),
+        cpi_accounts,
+        signer,
+    );
+    anchor_spl::token_2022::mint_to(cpi_ctx, amount)?;
+
+    // Update the vault state
+    ctx.accounts.vault.tokens_issued += amount;
 
     Ok(())
 }
--- programs/copy-trading/tests/deposit.spec.ts
+++ programs/copy-trading/tests/deposit.spec.ts
@@ -1,6 +1,11 @@
 import * as anchor from '@coral-xyz/anchor';
 import { createVault } from './helpers';
-import { getAssociatedTokenAddressSync } from '@solana/spl-token';
+import {
+  ASSOCIATED_TOKEN_PROGRAM_ID,
+  getAssociatedTokenAddressSync,
+  TOKEN_2022_PROGRAM_ID,
+  TOKEN_PROGRAM_ID
+} from '@solana/spl-token';
 
 describe('Deposit Instruction', () => {
   it('should allow deposits of sol', async () => {
@@ -28,15 +33,80 @@ describe('Deposit Instruction', () => {
         investor: investor.publicKey,
         operator: operator.publicKey,
         vault: vaultPubkey,
+        mint: mintPubkey,
+        mintTo: getAssociatedTokenAddressSync(
+          mintPubkey,
+          investor.publicKey,
+          false, // allowOwnerOffCurve parameter (optional)
+          TOKEN_2022_PROGRAM_ID // Specify the token program explicitly
+        ),
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+        tokenProgram: TOKEN_2022_PROGRAM_ID,
         systemProgram: anchor.web3.SystemProgram.programId,
+        rent: anchor.web3.SYSVAR_RENT_PUBKEY,
       })
       .signers([investor])
       .rpc();
 
-    // check investor's ata balance
+    // check vault sol balance
     const currentBalance = await provider.connection.getBalance(vaultPubkey);
-
     expect(currentBalance).toEqual(initialBalance + depositAmount.toNumber());
+  });
+
+  it('should mint tokens to investor', async () => {
+    // create vault
+    const { provider, program, operator, vaultPubkey, mintPubkey } =
+      await createVault();
+
+    // create investor and airdrop some SOL
+    const investor = anchor.web3.Keypair.generate();
+    const airdropSignature = await provider.connection.requestAirdrop(
+      investor.publicKey,
+      anchor.web3.LAMPORTS_PER_SOL * 2,
+    );
+    await provider.connection.confirmTransaction(airdropSignature);
+
+    // amount to deposit in vault
+    const depositAmount = new anchor.BN(0.2 * anchor.web3.LAMPORTS_PER_SOL);
+
+    console.log('>>>> here');
+    try {
+      const depositTx = await program.methods
+        .deposit(depositAmount)
+        .accounts({
+          investor: investor.publicKey,
+          operator: operator.publicKey,
+          vault: vaultPubkey,
+          mint: mintPubkey,
+          mintTo: getAssociatedTokenAddressSync(
+            mintPubkey,
+            investor.publicKey,
+            false, // allowOwnerOffCurve parameter (optional)
+            TOKEN_2022_PROGRAM_ID // Specify the token program explicitly
+          ),
+          associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+          tokenProgram: TOKEN_2022_PROGRAM_ID,
+          systemProgram: anchor.web3.SystemProgram.programId,
+          rent: anchor.web3.SYSVAR_RENT_PUBKEY,
+        })
+        .signers([investor])
+        .rpc();
+      console.log('Deposit Tx Signature', depositTx);
+    } catch (error) {
+      console.log('Error', error);
+      if (error?.getLogs) console.log('Error', await error.getLogs());
+
+      throw error;
+    }
+
+    // check investor token balance
+    const investorTokenAccount = getAssociatedTokenAddressSync(
+      mintPubkey,
+      investor.publicKey,
+    );
+    console.log('investorTokenAccount', investorTokenAccount.toBase58());
+
+
   });
 
 
